36c851e640daa0787ce2d80b21dd99853f4c2425
wangyouliang <1216027665@qq.com>
2019-10-06 15:41:55 +0800

    3-2 axios 封装 & 数据获取

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
   </div>
 </template>
 
 <script>
-import VHeader from './components/v-header/v-header'
+  import VHeader from './components/v-header/v-header'
+  import { getSeller } from 'api'
 
-export default {
-  name: 'app',
-  components: {
-    VHeader
+  export default {
+    name: 'app',
+    data() {
+      return {
+        seller: {}
+      }
+    },
+    created() {
+      this._getSeller()
+    },
+    methods: {
+      _getSeller() {
+        getSeller().then((seller) => {
+          this.seller = seller
+        })
+      }
+    },
+    components: {
+      VHeader
+    }
   }
-}
 </script>
 
 <style lang="stylus">
